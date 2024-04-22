b4034cafdc4875d041ab2602025a0f570a39135d
Thalins Genohan <thalinsgenohan@gmail.com>
2024-04-22 03:01:45 -0400

    Update common.js

--- scripts/common.js
+++ scripts/common.js
@@ -5,18 +5,22 @@ function getRandomNumber(min, max) {
 if (localStorage.lightning === undefined) localStorage.lightning = false; // default false
 if (localStorage.rain === undefined) localStorage.rain = true; // default true
 
-fetch("/assets/templates/navbar.html")
-    .then(data => { return data.text(); })
-    .then(data => { document.getElementById("navbar").innerHTML = data; });
-
-fetch("/assets/templates/settings.html")
-    .then(data => { return data.text(); })
-    .then(data => { document.getElementById("settings").innerHTML = data; })
-    .then(() => {
-        document.getElementById("lightning-toggle").checked = localStorage.lightning;
-        document.getElementById("rain-toggle").checked = localStorage.rain;
-    });
-
-fetch("/assets/templates/last-updated.html")
-    .then(data => { return data.text(); })
-    .then(data => { document.getElementById("last-updated").innerHTML = data; });
+if (document.getElementById("navbar") !== null) {
+    fetch("/assets/templates/navbar.html")
+        .then(data => { return data.text(); })
+        .then(data => { document.getElementById("navbar").innerHTML = data; });
+}
+if (document.getElementById("settings") !== null) {
+    fetch("/assets/templates/settings.html")
+        .then(data => { return data.text(); })
+        .then(data => { document.getElementById("settings").innerHTML = data; })
+        .then(() => {
+            document.getElementById("lightning-toggle").checked = localStorage.lightning;
+            document.getElementById("rain-toggle").checked = localStorage.rain;
+        });
+}
+if (document.getElementById("last-updated") !== null) {
+    fetch("/assets/templates/last-updated.html")
+        .then(data => { return data.text(); })
+        .then(data => { document.getElementById("last-updated").innerHTML = data; });
+}
